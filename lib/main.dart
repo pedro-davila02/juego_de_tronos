bc93b266cbf692857148cfb27c543b9aa973d703
Pedro <pedrodavilarequena@gmail.com>
2025-01-25 11:25:49 +0100

    Actualizacion main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'providers/character_provider.dart';
 import 'screens/home_screen.dart';
+import 'providers/favorite_provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => CharacterProvider(),
-      child: MyApp(),
-    ),
-  );
+  runApp(GameOfThronesApp());
 }
 
-class MyApp extends StatelessWidget {
+class GameOfThronesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => FavoriteProvider(),
+      child: MaterialApp(
+        title: 'Game of Thrones Characters',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeScreen(),
+      ),
     );
   }
 }
