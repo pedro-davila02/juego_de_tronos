c702e23c3f3b08b1da2b69166fdc416869e6f20b
Pedro <pedrodavilarequena@gmail.com>
2025-01-25 11:19:28 +0100

    Actualizacion favorites_screen.dart

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,29 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/character_provider.dart';
+import '../providers/favorite_provider.dart';
+import '../widgets/character_card.dart';
 
 class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<CharacterProvider>(context);
+    final favorites = Provider.of<FavoriteProvider>(context).favorites;
 
     return Scaffold(
       appBar: AppBar(title: Text('Favoritos')),
-      body: ListView.builder(
-        itemCount: provider.favorites.length,
-        itemBuilder: (context, index) {
-          final character = provider.favorites[index];
-          return ListTile(
-            title: Text(character.name),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                provider.removeFromFavorites(character);
+      body: favorites.isEmpty
+          ? Center(child: Text('No hay personajes favoritos.'))
+          : ListView.builder(
+              itemCount: favorites.length,
+              itemBuilder: (context, index) {
+                return CharacterCard(character: favorites[index]);
               },
             ),
-          );
-        },
-      ),
     );
   }
 }
