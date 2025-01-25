f2f608f25811bf6d2542f30739277b38284575a0
Pedro <pedrodavilarequena@gmail.com>
2025-01-25 11:17:11 +0100

    Actualizacion home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'featured_screen.dart';
 import 'list_screen.dart';
 import 'favorites_screen.dart';
-import 'all_characters_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,10 +12,9 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
   final List<Widget> _screens = [
-    FeaturedScreen(),        // Pantalla Destacado
-    ListScreen(),            // Pantalla Listado
-    FavoritesScreen(),       // Pantalla Favoritos
-    AllCharactersScreen(),   // Pantalla Todos los Personajes
+    FeaturedScreen(),
+    ListScreen(),
+    FavoritesScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -30,14 +28,22 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: _screens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Text('🏠'), label: 'Destacado'),
-          BottomNavigationBarItem(icon: Text('📜'), label: 'Listado'),
-          BottomNavigationBarItem(icon: Text('❤️'), label: 'Favoritos'),
-          BottomNavigationBarItem(icon: Text('📚'), label: 'Todos'),
-        ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Destacado',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Listado',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoritos',
+          ),
+        ],
       ),
     );
   }
