e281ba5fd1c5ebfa73d7345138d9d43bcbe65600
Pedro <pedrodavilarequena@gmail.com>
2025-01-28 20:55:25 +0100

    Update character.dart

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -3,23 +3,29 @@ class Character {
   final String gender;
   final String culture;
   final String born;
-  final List<String> aliases;
+  final String alias; // Alias principal
+  final List<String> aliases; // Lista completa de alias
 
   Character({
     required this.name,
     required this.gender,
     required this.culture,
     required this.born,
+    required this.alias,
     required this.aliases,
   });
 
   factory Character.fromJson(Map<String, dynamic> json) {
+    final aliasesList = List<String>.from(json['aliases'] ?? []);
     return Character(
-      name: json['name'] ?? 'Desconocido',
+      name: (json['name'] as String?)?.isNotEmpty == true
+          ? json['name']
+          : (aliasesList.isNotEmpty ? aliasesList[0] : 'Desconocido'),
       gender: json['gender'] ?? 'Desconocido',
       culture: json['culture'] ?? 'Desconocido',
       born: json['born'] ?? 'Desconocido',
-      aliases: List<String>.from(json['aliases'] ?? []),
+      alias: aliasesList.isNotEmpty ? aliasesList[0] : 'Desconocido',
+      aliases: aliasesList,
     );
   }
 
@@ -33,6 +39,7 @@ class Character {
         gender == otherCharacter.gender &&
         culture == otherCharacter.culture &&
         born == otherCharacter.born &&
+        alias == otherCharacter.alias &&
         aliases.length == otherCharacter.aliases.length &&
         aliases.every((alias) => otherCharacter.aliases.contains(alias));
   }
@@ -43,5 +50,6 @@ class Character {
       gender.hashCode ^
       culture.hashCode ^
       born.hashCode ^
+      alias.hashCode ^
       aliases.hashCode;
 }
